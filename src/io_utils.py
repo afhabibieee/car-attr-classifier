d8da5d3d52b9554f213dc4f963eb7ab5128a3272
My name <me@example.com>
2022-11-22 09:56:24 +0700

    fixed error type for argument type

--- src/io_utils.py
+++ src/io_utils.py
@@ -12,8 +12,8 @@ def parse_args():
     parser.add_argument('--n_val_task',     type=int,   default=configs.N_TRAINING_EPISODES,    help='number of task for meta validation')
     parser.add_argument('--n_worker',       type=int,   default=configs.N_WORKERS,              help='number of concurrent processing')
     parser.add_argument('--epochs',         type=int,   default=configs.EPOCHS,                 help='number of passes of the entire training')
-    parser.add_argument('--lr',             default=configs.LEARNING_RATE,          help='step size at each iteration while moving toward a minimum of a loss function')
-    parser.add_argument('--wd',             default=configs.WEIGHT_DECAY,           help='decreasing the learning rate during training')
+    parser.add_argument('--lr',             type=float, default=configs.LEARNING_RATE,          help='step size at each iteration while moving toward a minimum of a loss function')
+    parser.add_argument('--wd',             type=float, default=configs.WEIGHT_DECAY,           help='decreasing the learning rate during training')
     parser.add_argument('--backbone_name',  default='convnet',                      help='convnet/effinet/resnet')
     parser.add_argument('--variant_depth',  default=None,                           help='depth variation of the selected backbone (ex: \'s\' for effinet)')
 
