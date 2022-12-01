315b482d67338bfde5c20525b78dc1ca63610c28
afhabibieee <adil.99afh@gmail.com>
2022-12-01 22:06:35 +0700

    modified eval & added precision recall

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,12 @@
-import torch
+import time, torch
 from tqdm import tqdm
 from configs import DEVICE
 
 import mlflow
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import precision_score, recall_score
 
 def get_experiment_id(name):
     exp = mlflow.get_experiment_by_name(name)
@@ -96,14 +100,20 @@ def evaluate_per_task(
     total = len(query_labels)
     return classification_scores, correct, total 
 
-def evaluate(model, data_loader):
+def evaluate(model, data_loader, datasets):
     total_pred = 0
     correct_pred = 0
+    results = []
+    results.append(
+        'Ground truth,Predicted,1st prob,2nd prob,Top distance score'
+    )
+    times = 0
 
     model.to(DEVICE)
     model.eval()
     with torch.no_grad():
-        for support_images, support_labels, query_images, query_labels, _ in data_loader:
+        for support_images, support_labels, query_images, query_labels, class_id in data_loader:
+            start = time.time()
             classification_scores, correct, total = evaluate_per_task(
                 model,
                 support_images.to(DEVICE),
@@ -111,6 +121,52 @@ def evaluate(model, data_loader):
                 query_images.to(DEVICE),
                 query_labels.to(DEVICE)
             )
+            end = time.time()
+            times += (end - start)
             correct_pred += correct
             total_pred += total
-    return correct_pred/total_pred
+
+            top_score, pred_labels = torch.max(
+                classification_scores.data, 1
+            )
+            
+            first_prob, second_prob = zip(*torch.topk(classification_scores.softmax(-1).data, 2, 1)[0])
+            first_prob = torch.stack(first_prob, dim=0)
+            second_prob = torch.stack(second_prob, dim=0)
+
+            for i in range(len(query_labels)):
+                result = '{},{},{},{},{}'.format(
+                    datasets.class_names[class_id[query_labels[i]]],
+                    datasets.class_names[class_id[pred_labels[i]]],
+                    first_prob[i],
+                    second_prob[i],
+                    top_score[i]
+                )
+                results.append(result)
+
+    avg_accuracy = correct_pred/total_pred
+    avg_time = times / len(data_loader)
+    return avg_accuracy, results, avg_time
+
+
+def precision_recall_curve(path, thresholds):
+    results = pd.read_csv(path)
+
+    y_true = ["known" if value else "unknown" for value in results['Ground truth'] == results['Predicted']]
+    pred_scores = results['1st prob'].values
+
+    precisions = []
+    recalls = []
+    
+    for threshold in thresholds:
+        y_pred = ["known" if score >= threshold else "unknown" for score in pred_scores]
+
+        precision = precision_score(y_true=y_true, y_pred=y_pred, pos_label="known")
+        recall = recall_score(y_true=y_true, y_pred=y_pred, pos_label="known")
+        
+        precisions.append(precision)
+        recalls.append(recall)
+
+    f1 = 2 * ((np.array(precisions) * np.array(recalls)) / (np.array(precisions) + np.array(recalls)))
+
+    return precisions, recalls, f1
